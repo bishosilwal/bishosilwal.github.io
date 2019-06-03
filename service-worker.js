d44cf74c4dd6ccdf0fc6a68c92121438de8c8afc
bishosilwal <aayussilwal@gmail.com>
2019-06-03 14:52:59 +0545

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.786475126c7adab6a47cd52a90cce7f3.js"
 );
 
 self.addEventListener('message', (event) => {
