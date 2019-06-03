db967cef3fa2936d131933e644cdd402e7bba5bc
bishosilwal <aayussilwal@gmail.com>
2019-06-03 16:25:46 +0545

    about me added

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import image2 from '../images/image2.png'
 
 export default class Body extends Component{
 
@@ -9,9 +10,9 @@ export default class Body extends Component{
         <div className="row body-row">
           <div className="col-6 d-flex">
             <div className="align-self-center">
-              <h5>Hey There</h5>
-              <h2>I AM BISHO SILWAL </h2>
-              <h4>CREATIVE FULL STACK WEB DEVELOPER</h4>
+              <h5 className="text-black-50 font-weight-light">Hey There</h5>
+              <h2 className='primary_color'>I AM BISHO SILWAL </h2>
+              <h4 className="text-black-50 font-weight-light">CREATIVE FULL STACK WEB DEVELOPER</h4>
 
               <button className="btn btn_primary">SEE MY WORK </button>
             </div>
@@ -20,21 +21,24 @@ export default class Body extends Component{
             <img src="https://colorlib.com/preview/theme/breed2/img/banner/home-right.png"/>
           </div>
         </div>
-        <div className="row about-myself-row">
-          <div className="col-6">
-
+        <div className="row about-myself-row pt-5">
+          <div className="col-6 image-div d-flex justify-content-center">
+            <div className="align-self-center">
+              <img src="https://colorlib.com/preview/theme/breed2/img/banner/home-right.png"/>
+            </div>
           </div>
-          <div className="col-6">
-            <h2>ABOUT MYSELF </h2>
-            <p>
-              inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed. inappropriate behavior is often laughed off as “boys will be boys,” women face higher.
-              That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed.
-            </p>
-            <button type="button" class="btn btn-primary mr-2">More Info</button>
-            <button type="button" class="btn btn-danger">Resume</button>
+          <div className="col-6 d-flex">
+            <div className="align-self-center">
+              <h5 className="text-black-50">ABOUT ME</h5>
+              <h2 className='primary_color'>CREATIVE FULL STACK WEB DEVELOPER</h2>
+              <p className="font-weight-light text-black-50">
+                My name is Bisho Silwal and I am a freelance web developer who specializes in creating dynamic and
+                beautiful web application using latest tech stack. I have been in the field for nearly 2 years, and have been loving every minute of it. Check out some of the links below to see what I’ve been up to lately
+              </p>
+            </div>
           </div>
         </div>
-        <div className="row about-myself-row">
+        <div className="row about-myself-row pt-5">
           <div className="col-12">
             <h2>WHAT SERVICE I OFFER FOR YOU</h2>
           </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
 
 export default class Header extends Component{
+
+  state = {
+    scroll: false
+  }
+  componentDidMount() {
+      window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = (event)=>{
+    if(window.scrollY > 10)
+    {
+      this.setState({scroll: true});
+    }else{
+      this.setState({scroll: false});
+    }
+  }
+
+  getBgColor = () => {
+    let scroll = this.state.scroll
+    return scroll ? 'bg-white shadow' : '';
+  }
   render(){
+    const { scroll } = this.state
     return(
-        <nav className="navbar navbar-expand-lg navbar-light sticky-top">
+        <nav className={"navbar navbar-expand-lg navbar-light sticky-top "+ this.getBgColor()}>
           <div className="container">
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
